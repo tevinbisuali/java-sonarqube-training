208ae07ddae6ebac34319d18564050694d74f12e
tevinbisuali <tevincoven1@gmail.com>
2026-06-03 22:49:30 +0000

    Add duplicated code for SonarQube test

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,13 +1,28 @@
 package com.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    public static void main(String[] args) {
+        String password = "admin123";
+        System.out.println(password);
+
+        printUserInfo();
+        printAdminInfo();
+    }
+
+    public static void printUserInfo() {
+        System.out.println("Name: John");
+        System.out.println("Role: User");
+        System.out.println("Access: Basic");
+        System.out.println("Department: IT");
+        System.out.println("Location: Dallas");
+    }
+
+    public static void printAdminInfo() {
+        System.out.println("Name: John");
+        System.out.println("Role: User");
+        System.out.println("Access: Basic");
+        System.out.println("Department: IT");
+        System.out.println("Location: Dallas");
     }
 }
